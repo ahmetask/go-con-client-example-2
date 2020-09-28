54b00084ad4c02a1b4fdd600feda056ab49ae341
Ahmet Aşık <ahmet9417@gmail.com>
2020-09-28 17:25:44 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 docker build -t go-con-client-2 .
 kubectl delete deployment gocon-client-2
 kubectl delete service gocon-client-2
-kubectl apply -f deployment.yml
-kubectl apply -f service.yml
+kubectl create -f deployment.yml
+kubectl create -f service.yml
